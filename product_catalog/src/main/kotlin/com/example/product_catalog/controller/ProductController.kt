266077358961c0dbe1c78a90c92b353bd713a1be
Manaswita Mandal <96064243+Manaswita06@users.noreply.github.com>
2024-10-22 12:27:57 +0530

    add web client

--- product_catalog/src/main/kotlin/com/example/product_catalog/controller/ProductController.kt
+++ product_catalog/src/main/kotlin/com/example/product_catalog/controller/ProductController.kt
@@ -2,21 +2,29 @@ package com.example.product_catalog.controller
 
 import com.example.product_catalog.model.Product
 import com.example.product_catalog.service.ProductService
+import com.example.product_catalog.webclient.HttpService
+import jakarta.servlet.http.HttpServletRequest
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/products")
-class ProductController(private val productService: ProductService) {
+class ProductController(private val productService: ProductService, private val httpService: HttpService) {
 
     @PostMapping
-    fun addProduct(@RequestBody product: Product): ResponseEntity<Product> {
+    fun addProduct(http :HttpServletRequest, @RequestBody product: Product): ResponseEntity<Product> {
+        var header = http.getHeader("Authorization")
+        var username = httpService.httpCallService(header.substring(7))
+        product.seller_name = username
         return ResponseEntity.ok(productService.addProduct(product))
     }
 
     @PutMapping("/{id}")
-    fun updateProduct(@PathVariable id: String, @RequestBody product: Product): ResponseEntity<Product?> {
+    fun updateProduct(http :HttpServletRequest, @PathVariable id: String, @RequestBody product: Product): ResponseEntity<Product?> {
         val updatedProduct = productService.updateProduct(id, product)
+        var header = http.getHeader("Authorization")
+        var username = httpService.httpCallService(header.substring(7))
+        product.seller_name = username
         return if (updatedProduct != null) {
             ResponseEntity.ok(updatedProduct)
         } else {
@@ -25,19 +33,25 @@ class ProductController(private val productService: ProductService) {
     }
 
     @DeleteMapping("/{id}")
-    fun deleteProduct(@PathVariable id: String): ResponseEntity<Void> {
+    fun deleteProduct(http :HttpServletRequest, @PathVariable id: String): ResponseEntity<Void> {
+        var header = http.getHeader("Authorization")
+        var username = httpService.httpCallService(header.substring(7))
         productService.deleteProduct(id)
         return ResponseEntity.noContent().build()
     }
 
     @GetMapping
-    fun getAllProducts(): ResponseEntity<List<Product>> {
+    fun getAllProducts(http :HttpServletRequest): ResponseEntity<List<Product>> {
+        var header = http.getHeader("Authorization")
+        var username = httpService.httpCallService(header.substring(7))
         return ResponseEntity.ok(productService.getAllProducts())
     }
 
     @GetMapping("/{id}")
-    fun getProductById(@PathVariable id: String): ResponseEntity<Product?> {
+    fun getProductById(http :HttpServletRequest, @PathVariable id: String): ResponseEntity<Product?> {
         val product = productService.getProductById(id)
+        var header = http.getHeader("Authorization")
+        var username = httpService.httpCallService(header.substring(7))
         return if (product != null) {
             ResponseEntity.ok(product)
         } else {
--- product_catalog/src/main/kotlin/com/example/product_catalog/model/Product.kt
+++ product_catalog/src/main/kotlin/com/example/product_catalog/model/Product.kt
@@ -7,6 +7,7 @@ data class Product(
     @JsonProperty("_id") var id: String? = null,      // CouchDB's _id field
     @JsonProperty("_rev") var revision: String? = null, // CouchDB's _rev field
     @JsonProperty("name") var name: String,            // Added @JsonProperty annotation
+    @JsonProperty("seller_name") var seller_name: String,
     @JsonProperty("price") var price: Double,          // Added @JsonProperty annotation
     @JsonProperty("quantity") var quantity: Int,       // Added @JsonProperty annotation
     @JsonProperty("description") var description: String? = null // Added @JsonProperty annotation
